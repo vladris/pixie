5197a1f6d184b62aa01ee6d294763f0eaafa83d5
Vlad Riscutia <vladris@outlook.com>
2017-04-01 18:50:26 -0700

    Rewire assembler with saner implementation

--- dust/dust.py
+++ dust/dust.py
@@ -13,78 +13,51 @@ content = [' '.join(line.split()) for line in content]
 # strip blank lines and comments, then split tokens
 content = [line.split() for line in content if line and line[0] != '#']
 
-# token is number if a label or (ignoring dereferences) starts with a digit
-def is_number(token):
-    if token[0] == ':':
-        # labels resolve to numbers
-        return True
-    if token[0] == '*':
-        # ignore dereference
-        token = token[1:]
-    return token[0].isdigit()
-
-# convert numbers to string in bases 10, 2, and 16
-def atoi(token):
-    if 'x' in token:
-        return int(token, 16)
-    elif 'b' in token:
-        return int(token, 2)
-    else:
-        return int(token)
-
-# compute offset of each label
-labels, i, offset = { }, 0, -1
-for line in content:
-    if line[0][-1] == ':':
-        # if label, associate it with offset of next instruction
-        labels[':' + line[0][:-1]] = str(offset + 1)
-    else:
-        # else increase offset by number of non-register non-opcode tokens 
-        offset += len([1 for token in line if is_number(token)])
-        # if this starts with an opcode, add 1 for the instruction itself
-        if not is_number(line[0]):
-            offset += 1
-
-# strip labels
-content = [line for line in content if line[0][-1] != ':']
-
-# replace labels in instructions by their actual offset
-content = [
-    [labels[token] if token in labels else token for token in line]
-    for line in content]
-
 op_codes = ['mov', 'add', 'sub', 'mul', 'div', 'rem', 'not', 'and', 'or',
     'xor', 'eq', 'le', 'leq', 'jnz', 'in', 'out']
-registers = ['r0', 'r1', 'r2', 'r3', 'sb', 'sp', 'pc']
+registers = ['r0', 'r1', 'r2', 'r3', 'sb', 'sp', 'pc', '__value', '*r0',
+    '*r1', '*r2', '*r3', '*sb', '*sp', '*pc', '*__value']
 
-# add operand to encoded word
-def encode(encoded, operand, shift=0):
-    if operand[0] == '*':
-        # mark dereference (0b1000)
-        encoded[0] |= 8 << shift
-        operand = operand[1:]
+# encode operand
+def op(token):
+    if token not in registers:
+        # if not a register, replace with '__value', keeping dereference prefix
+        token = ('*' if token[0] == 0 else '') + '__value'
+    # encoded operand is index in register list
+    return registers.index(token)
 
-    if operand not in registers:
-        # if this is a number, operand is 0b111 and add value as next word
-        encoded[0] |= 7 << shift
-        return encoded + [atoi(operand)]
+# encode value
+def value(token):
+    # separate word value exists only if token is not a register
+    return [token.strip('*')] if token not in registers else []
+
+# parse line to its encoded representation
+def parse(line):
+    if line[0].isdigit():
+        # for numbers, simply return them
+        return line
+    elif line[0] in op_codes:
+        # for instruction, expand it to op code, encoded operands, and values
+        expanded = [op_codes.index(line[0]), op(line[1]),
+            op(line[2])] + value(line[1]) + value(line[2])
+        # then encode op code and operands into a single word
+        return [int(expanded[0]) << 8 | int(expanded[1]) << 4 |
+            int(expanded[2])] + expanded[3:]
     else:
-        # else encode register
-        encoded[0] |= registers.index(operand) << shift
-        return encoded
+        # otherwise we have a label, leave it be for now 
+        return [line[0]]
 
-# encode program
-program = []
-for line in content:
-    if is_number(line[0]):
-        # if line starts with a number it should only contain numbers 
-        program += [atoi(token) for token in line]
+# parse lines, determine index of each label
+program, labels, offset = [], {}, 0
+for token in sum([parse(line) for line in content], []):
+    if isinstance(token, str) and token[-1] == ':':
+        labels[':' + token[:-1]] = offset
     else:
-        # encode opcode and operands
-        code = [op_codes.index(line[0]) << 8]
-        code = encode(code, line[1], 4)
-        code = encode(code, line[2])
-        program += code
+        offset += 1
+        program.append(token)
+
+# replace labels with their offset
+program = [labels[token] if token in labels else token for token in program]
 
 with open(sys.argv[2], "w") as f:
-    f.write(' '.join([str(instr) for instr in program]))
+    f.write(' '.join([str(int(str(instr), 0)) for instr in program]))
