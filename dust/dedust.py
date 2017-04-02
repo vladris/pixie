8b3bafff99f9a51100d466a9b9082a15efd3356f
Vlad Riscutia <vladris@outlook.com>
2017-04-02 10:59:29 -0700

    If dedust can't decode instruction, assume it is data

--- dust/dedust.py
+++ dust/dedust.py
@@ -9,9 +9,13 @@ registers = ['r0', 'r1', 'r2', 'r3', 'sb', 'sp', 'pc', '__value', '*r0',
     '*r1', '*r2', '*r3', '*sb', '*sp', '*pc', '*__value']
 
 def decode_instructions(instr, offset=0):
-    decoded = [op_codes[(instr[0] >> 8) & 0xFF],
-               registers[(instr[0] >> 4) & 0xF],
-               registers[instr[0] & 0xF]]
+    try:
+        decoded = [op_codes[(instr[0] >> 8) & 0xFF],
+                   registers[(instr[0] >> 4) & 0xF],
+                   registers[instr[0] & 0xF]]
+    except:
+        # if we can't decode the instruction, it's likely raw data
+        decoded = ['raw', 'data: ', '{0:#06x}'.format(instr[0])]
     instr, instr_start, raw = instr[1:], offset, [instr[0]]
 
     for operand in [1, 2]:
@@ -27,7 +31,7 @@ def decode_instructions(instr, offset=0):
     raw = '{0: <17}'.format(' '.join(['{0:#05}'.format(word) for word in raw]))
 
     # prints offset: decoded instruction | raw words
-    fmt = '{0:#06x}: {1: >3} {2: >6} {3: >6} |' + raw
+    fmt = '{0:#06x}: {1: >3} {2: >6} {3: >6} | ' + raw
     print(fmt.format(instr_start, *decoded))
     if instr:
         decode_instructions(instr, offset + 1)
