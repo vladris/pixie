64f399028bed1b09aa5ebdf2cec617de54bb92b8
Vlad Riscutia <vladris@outlook.com>
2018-08-06 09:03:15 -0700

    Retab

--- pixie/pixie.cpp
+++ pixie/pixie.cpp
@@ -6,36 +6,36 @@ using namespace pixie;
 
 std::vector<word_t> load_program(const std::string& file)
 {
-	std::vector<word_t> program;
-	std::fstream fs;
-	fs.open(file, std::fstream::in);
+    std::vector<word_t> program;
+    std::fstream fs;
+    fs.open(file, std::fstream::in);
 
-	while (!fs.eof())
-	{
-		word_t word;
-		fs >> word;
-		program.push_back(word);
-	}
+    while (!fs.eof())
+    {
+        word_t word;
+        fs >> word;
+        program.push_back(word);
+    }
 
-	fs.close();
-	return program;
+    fs.close();
+    return program;
 }
 
 int main(int argc, const char *argv[])
 {
-	if (argc != 2)
-	{
-		std::cout << "Usage: pixie <program>" << std::endl;
-		return 0;
-	}
+    if (argc != 2)
+    {
+        std::cout << "Usage: pixie <program>" << std::endl;
+        return 0;
+    }
 	
-	auto program = load_program(argv[1]);
+    auto program = load_program(argv[1]);
 
-	vm_t vm;
-	vm.connect_input([]() { word_t input; std::cin >> input; return input; });
-	vm.connect_output([](word_t output) { std::cout << output; });
+    vm_t vm;
+    vm.connect_input([]() { word_t input; std::cin >> input; return input; });
+    vm.connect_output([](word_t output) { std::cout << output; });
 
-	vm.run(program.begin(), program.end());
+    vm.run(program.begin(), program.end());
 	
-	return 0;
+    return 0;
 }
